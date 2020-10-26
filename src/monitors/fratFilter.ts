6fe33b63d937cefea7e8438b26ff6849b19740a9
graytonio <g-buddy@comcast.net>
2020-10-26 17:15:34 -0400

    Added frat filter

---
+++ src/monitors/fratFilter.ts
@@ -0,0 +1,24 @@
+import { botCache } from '../../mod.ts';
+import { deleteMessage } from '../../deps.ts';
+import { sendAlertResponse } from "../utils/helpers.ts";
+
+botCache.monitors.set("fratmonitor", {
+    name: "fratmonitor",
+    botChannelPermissions: ["MANAGE_MESSAGES"],
+    execute: async function (message) {
+        console.log(message);
+
+        const isInFratChannel = (message.channelID === "770341120999030845");
+        const isFratMessage = (message.content === "φρατ");
+
+        if(!isInFratChannel) return;
+        if(isFratMessage) return;
+
+        try {
+            deleteMessage(message);
+            sendAlertResponse(message, "That wasn't very FRAT of you");
+        } catch (error) {
+            
+        }
+    }
+})
